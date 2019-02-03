fcff0a5936557bf3c3750ea7bd982515ecfc52be
diogofreitas <diogom42@gmail.com>
2019-02-03 15:17:30 -0200

    Adds A problem's matrix files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Spectrum import Spectrum
 
 
 def main():
-    spect = Spectrum("", "45-A", "v3")
+    spect = Spectrum("", "45-A", "45-A-bug-9385104-9385119")
     print(spect.metrics[1])
 
 if __name__ == "__main__":
